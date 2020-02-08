e61018acb5944452af93f6d91902f511be5b5fa4
Georges Labreche <georges.labreche@gmail.com>
2020-02-09 00:32:20 +0100

    Created bash options for rasterization.

---
+++ makemap.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+#======================================================================================================
+# Name:         makemap.sh
+# Description:  Create files for a 3D model environment that can be loaded into the MARS simulator.
+# Dependencies: None.
+#======================================================================================================
+
+
+DIR_OUTPUT=output
+
+#########################
+# READ INPUT PARAMETERS #
+#########################
+while getopts ":h:d:r:t:i:" opt; do
+  case $opt in
+    h) size_height_map="$OPTARG"
+    ;;
+    d) size_diffusion_map="$OPTARG"
+    ;;
+    r) pdal_resolution="$OPTARG"
+    ;;
+    t) pdal_output_type="$OPTARG"
+    ;;
+    i) xyz_filename="$OPTARG"
+    ;;
+    \?) printf "\nInvalid option -$OPTARG\n\n" >&2
+    exit 1
+    ;;
+  esac
+done
+
+# Get name of xyz file without its extension.
+# Use this name as the directory name where created files will be outputted to.
+filename=$(basename -- "$xyz_filename")
+filename="${filename%.*}"
+
+# Check if options are set to blank.
+# Build the rasterize.sh option substrings that will be used to construct the bash script invokation string.
+if [ -z "$xyz_filename" ]
+then
+    xyz_filename=""
+else
+    xyz_filename="-i $xyz_filename"
+fi
+
+if [ -z "$size_height_map" ]
+then
+    size_height_map=""
+else
+    size_height_map="-h $size_height_map"
+fi
+
+if [ -z "$size_diffusion_map" ]
+then
+    size_diffusion_map=""
+else
+    size_diffusion_map="-d $size_diffusion_map"
+fi
+
+if [ -z "$pdal_resolution" ]
+then
+    pdal_resolution=""
+else
+    pdal_resolution="-r $pdal_resolution"
+fi
+
+if [ -z "$pdal_output_type" ]
+then
+    pdal_output_type=""
+else
+    pdal_output_type="-t $pdal_output_type"
+fi
+
+# Check if height and diffisusion map files have already been created.
+# If not, create them by invoking the rasterize.sh bash script.
+if [ ! -f "$DIR_OUTPUT/$filename/heightmap.png" ] || [ ! -f "$DIR_OUTPUT/$filename/diffusion.png" ] ; then
+    printf "\nRasterizing...\n"
+    bash rasterize.sh $size_height_map $size_diffusion_map $pdal_resolution $pdal_output_type $xyz_filename
+fi
+
+# TODO, sed template files and output them into new files.
+
+# README.md
+# manifest.xml
+# heightmap.yml
+# Shaders: vertex_shaders.yml and fragment_shader.yml
+# ground.bobj
--- rasterize.sh
+++ rasterize.sh
@@ -1,47 +1,126 @@
 #!/bin/bash
 #======================================================================================================
-# Name:         mapper.sh
+# Name:         rasterize.sh
 # Description:  Reads a DSM point cloud file and outputs height and diffusion map image files.
-#
 # Dependencies: LAStools, PDAL, GDAL, python-gdal, and ImageMagick.
-#               The following version were used while developing this script:
-#               - GDAL 2.2.3, released 2017/11/20
-#               - ImageMagick 6.9.7-4 Q16 x86_64 20170114
 #======================================================================================================
 
-# Requirements:
-# sudo apt install liblas-bin
-# http://lastools.org/
-# https://github.com/LAStools/LAStools
 
-# sudo apt install pdal
+################################
+# CONSTANT AND DEFAULT VALUES #
+################################
 
-# sudo apt install python-gdal
+# Flag to indicate if tmp files should be deleted at the end of the script.
+DELETE_TMP_FILES=true
 
 # Target sizes for height map and diffusion map image file outputs.
-SIZE_HEIGHT_MAP=512
-SIZE_DIFFUSION_MAP=4096
+DEFAULT_SIZE_HEIGHT_MAP=512
+DEFAULT_SIZE_DIFFUSION_MAP=4096
 
 DIR_TMP=tmp
 DIR_OUTPUT=output
+
+# Default PDAL pipeline configurations
+PDAL_DEFAULT_RESOLUTION=0.05
+PDAL_DEFAULT_OUTPUT_TYPE="mean"
+
+# Template PDAL pipeline files.
+PDAL_PIPELINE_DTM=templates/pipelines/dtm.json.template
+PDAL_PIPELINE_RGB=templates/pipelines/rgb.json.template
 
 # Expected dgalinfo string output pattern for size info.
 REGEX_SIZE=".*Size is ([0-9]+), ([0-9]+).*"
 
 
-###########
-# CLEANUP #
-###########
+#########################
+# READ INPUT PARAMETERS #
+#########################
+while getopts ":h:d:r:t:i:" opt; do
+  case $opt in
+    h) size_height_map="$OPTARG"
+    ;;
+    d) size_diffusion_map="$OPTARG"
+    ;;
+    r) pdal_resolution="$OPTARG"
+    ;;
+    t) pdal_output_type="$OPTARG"
+    ;;
+    i) xyz_filename="$OPTARG"
+    ;;
+    \?) printf "\nInvalid option -$OPTARG\n\n" >&2
+    exit 1
+    ;;
+  esac
+done
+
+# Check if input pointcloud filename was given.
+if [ -z "$xyz_filename" ]
+then
+    printf "\nUse the -i option to input the path to the .xyz file to process. \n\n"
+    exit 1
+fi
+
+# Set default height map size if not given as parameter.
+if [ -z "$size_height_map" ]
+then
+    size_height_map=$DEFAULT_SIZE_HEIGHT_MAP
+fi
+
+# Set default diffusion map size if not given as parameter.
+if [ -z "$size_diffusion_map" ]
+then
+    size_diffusion_map=$DEFAULT_SIZE_DIFFUSION_MAP
+fi
+
+# Set default pdal image processing resolution.
+if [ -z "$pdal_resolution" ]
+then
+    pdal_resolution=$PDAL_DEFAULT_RESOLUTION
+fi
+
+# Set default pdal output type.
+if [ -z "$pdal_output_type" ]
+then
+    pdal_output_type=$PDAL_DEFAULT_OUTPUT_TYPE
+fi
+
+
+###########################################
+# CREATE TEMPORARY AND OUTPUT DIRECTORIES #
+###########################################
+
+# Create parent tmp directory if it doesn't exist already.
+if [ ! -d $DIR_TMP ]; then mkdir $DIR_TMP; fi
+
+# Create parent output directory if it doesn't exist already.
+if [ ! -d $DIR_OUTPUT ]; then mkdir $DIR_OUTPUT; fi
+
+# Get name of xyz file without its extension.
+# Use this name as the directory name where created files will be outputted to.
+filename=$(basename -- "$xyz_filename")
+filename="${filename%.*}"
+
+target_tmp_dir="$DIR_TMP/$filename"
+target_output_dir="$DIR_OUTPUT/$filename"
 
 # Delete temporary files that the script creates.
 # They may be left over from abruptly stopping the script during a previous run.
-if [ -d $DIR_TMP ]; then rm -Rf $DIR_TMP; fi
+if [ -d $target_tmp_dir ]; then rm -Rf $target_tmp_dir; fi
 
-# Recreate the tmp directory where temporary files will be saved.
-mkdir $DIR_TMP
+# Create the tmp subdirectory where temporary files will be saved.
+mkdir $target_tmp_dir
 
-# Create output directory if it doesn't exist already.
-if [ ! -d output ]; then mkdir output; fi
+# Create output directory for the inputted file.
+if [ ! -d $target_output_dir ]; then mkdir $target_output_dir; fi
+
+
+###########################
+# DEFINE OUTPUT FILENAMES #
+###########################
+
+# Final outout files.
+heightmap_filename=$target_output_dir/heightmap.png
+diffusionmap_filename=$target_output_dir/diffusion.png
 
 
 ###################
@@ -50,14 +129,24 @@ if [ ! -d output ]; then mkdir output; fi
 
 # Use LAStools to parse the xyzRGB pointcloud ASCII format data into a laz format file.
 printf "\nParsing xyzRGB pointcloud file to LAZ...\n"
-txt2las -parse xyzRGB -i $1 -o $DIR_TMP/pointcloud.laz
+
+# Laz filename needs to match filename in pipeline json templates.
+txt2las -parse xyzRGB -i $xyz_filename -o $target_tmp_dir/pointcloud.laz
+
+# Create the GTiff DTM file...
+printf "Creating GTiff DTM...\n"
+
+# PDAL pipeline definition filename.
+dtm_pipeline_filename=$target_tmp_dir/dtm.json
+
+# Create the DTM pipeline JSON file.
+sed "s/{RESOLUTION}/$pdal_resolution/g" $PDAL_PIPELINE_DTM | sed "s/{OUTPUT_TYPE}/$pdal_output_type/g" | sed "s/{OUTPUT_TYPE}/$pdal_output_type/g" | sed "s/{TMP_DIR}/${target_tmp_dir/\//\\\/}/g" > $dtm_pipeline_filename
 
 # Use PDAL to create the GTiff DTM file.
-printf "Creating GTiff DTM...\n"
-pdal pipeline pipelines/dtm.json
+pdal pipeline $dtm_pipeline_filename
 
 # Use GDAL to fetch the size of the DTM.
-gdalinfo_stdout="$(gdalinfo $DIR_TMP/dtm.tif 2>&1)"
+gdalinfo_stdout="$(gdalinfo "$target_tmp_dir/dtm.tif" 2>&1)"
 
 
 ##################
@@ -76,15 +165,15 @@ fi
 # DEFINE SIZE OF OUTPUT FILES #
 ###############################
 
-# Define the size of the heightmap output.
-# Either width or height needs to be set to SIZE_HEIGHT_MAP.
+# Define the size of the heightmap and diffusion map outputs.
+# Either width or height needs to be set to size_height_map/size_diffusion_map.
 # Pick the one that is the longest from the DTM size that was fetched earlier.
 if [ "$width" -gt "$height" ]; then
-    heightmap_outsize="$SIZE_HEIGHT_MAP 0"
-    diffusionmap_outsize="$SIZE_DIFFUSION_MAP 0"
+    heightmap_outsize="$size_height_map 0"
+    diffusionmap_outsize="$size_diffusion_map 0"
 else
-    heightmap_outsize="0 $SIZE_HEIGHT_MAP"
-    diffusionmap_outsize="0 $SIZE_DIFFUSION_MAP"
+    heightmap_outsize="0 $size_height_map"
+    diffusionmap_outsize="0 $size_diffusion_map"
 fi
 
 
@@ -96,11 +185,11 @@ fi
 printf "Creating grayscale heightmap...\n"
 
 # Use GDAL to create grayscale heightmap file.
-gdal_translate -of PNG -ot Byte -scale -b 1 $DIR_TMP/dtm.tif $DIR_OUTPUT/heightmap.png -outsize $heightmap_outsize
+gdal_translate -of PNG -ot Byte -scale -b 1 -outsize $heightmap_outsize "$target_tmp_dir/dtm.tif" $heightmap_filename
 
-# Use ImageMagick to rescale canvas size to SIZE_HEIGHT_MAP x SIZE_HEIGHT_MAP pixels.
-printf "Rescaling heightmap canvas to ${SIZE_HEIGHT_MAP}, ${SIZE_HEIGHT_MAP}...\n"
-convert $DIR_OUTPUT/heightmap.png -resize "${SIZE_HEIGHT_MAP}x${SIZE_HEIGHT_MAP}" -background Black -gravity center -extent "${SIZE_HEIGHT_MAP}x${SIZE_HEIGHT_MAP}" $DIR_OUTPUT/heightmap.png
+# Use ImageMagick to rescale canvas size to size_height_map x size_height_map pixels.
+printf "Rescaling heightmap canvas to ${size_height_map}, ${size_height_map}...\n"
+convert $heightmap_filename -resize "${size_height_map}x${size_height_map}" -background Black -gravity center -extent "${size_height_map}x${size_height_map}" $heightmap_filename
 
 
 ##########################
@@ -110,24 +199,36 @@ convert $DIR_OUTPUT/heightmap.png -resize "${SIZE_HEIGHT_MAP}x${SIZE_HEIGHT_MAP}
 # Verbosity.
 printf "Creating RGB diffusionmap...\n"
 
-# Use PDAL to create the GTiff DTM file.
-pdal pipeline pipelines/rgb.json
+# PDAL pipeline definition filename.
+rgb_pipeline_filename=$target_tmp_dir/rgb.json
+
+# Create the DTM pipeline JSON file.
+sed "s/{RESOLUTION}/$pdal_resolution/g" $PDAL_PIPELINE_RGB | sed "s/{OUTPUT_TYPE}/$pdal_output_type/g" | sed "s/{TMP_DIR}/${target_tmp_dir/\//\\\/}/g" > $rgb_pipeline_filename
+
+# Use PDAL to create the RGB GTiff DTM file.
+pdal pipeline $rgb_pipeline_filename
 
 # Use python-gdal to merge red, green, and blue tif files into a single rbg.tif file.
 # This could probably have been done in the PDAL rgb pipeline filters.merge but it throws a malloc error for high resolutions.
-gdal_merge.py -separate $DIR_TMP/red.tif $DIR_TMP/green.tif $DIR_TMP/blue.tif -o $DIR_TMP/rgb.tif
+# The name of the tif files that are being merged are set in the the rgb.json.template file.
+gdal_merge.py -separate $target_tmp_dir/red.tif $target_tmp_dir/green.tif $target_tmp_dir/blue.tif -o $target_tmp_dir/rgb.tif
 
 # Use GDAL to create the RGB diffusion map.
-gdal_translate -of PNG -ot Byte -scale -b 1 -b 2 -b 3 $DIR_TMP/rgb.tif $DIR_OUTPUT/diffusion.png -outsize $diffusionmap_outsize
+gdal_translate -of PNG -ot Byte -scale -b 1 -b 2 -b 3 -outsize $diffusionmap_outsize $target_tmp_dir/rgb.tif $diffusionmap_filename
 
-# Use ImageMagick to rescale canvas size to SIZE_DIFFUSION_MAP x SIZE_DIFFUSION_MAP pixels.
-printf "Rescaling heightmap canvas to ${SIZE_DIFFUSION_MAP}, ${SIZE_DIFFUSION_MAP}...\n"
-convert $DIR_OUTPUT/diffusion.png -resize "${SIZE_DIFFUSION_MAP}x${SIZE_DIFFUSION_MAP}" -background Black -gravity center -extent "${SIZE_DIFFUSION_MAP}x${SIZE_DIFFUSION_MAP}" $DIR_OUTPUT/diffusion.png
+# Use ImageMagick to rescale canvas size to size_diffusion_map x size_diffusion_map pixels.
+printf "Rescaling heightmap canvas to ${size_diffusion_map}, ${size_diffusion_map}...\n"
+convert $diffusionmap_filename -resize "${size_diffusion_map}x${size_diffusion_map}" -background Black -gravity center -extent "${size_diffusion_map}x${size_diffusion_map}" $diffusionmap_filename
 
 
 ###########
 # CLEANUP #
 ###########
 
-# Delete tmp directory containing all the temporary files created by the script.
-rm -Rf tmp
+# Delete all the temporary files created by the script.
+if [ $DELETE_TMP_FILES = true ] ; then
+    if [ -d $target_tmp_dir ]; then rm -Rf $target_tmp_dir; fi
+
+    # Remove auxiliary files.
+    rm $target_output_dir/*.aux.xml
+fi

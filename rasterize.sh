4ce0fa01f810bdcc42e304988b7360a197f9be37
Georges Labrèche <georges@opendatakosovo.org>
2020-02-08 01:44:16 +0100

    Now using LAStools, PDAL, and python-gdal.

--- rasterize.sh
+++ rasterize.sh
@@ -1,103 +1,133 @@
 #!/bin/bash
 #======================================================================================================
-# Name:         rasterize.sh
+# Name:         mapper.sh
 # Description:  Reads a DSM point cloud file and outputs height and diffusion map image files.
 #
-# Dependencies: GDAL and ImageMagick.
+# Dependencies: LAStools, PDAL, GDAL, python-gdal, and ImageMagick.
 #               The following version were used while developing this script:
 #               - GDAL 2.2.3, released 2017/11/20
 #               - ImageMagick 6.9.7-4 Q16 x86_64 20170114
 #======================================================================================================
 
+# Requirements:
+# sudo apt install liblas-bin
+# http://lastools.org/
+# https://github.com/LAStools/LAStools
 
+# sudo apt install pdal
+
+# sudo apt install python-gdal
 
 # Target sizes for height map and diffusion map image file outputs.
 SIZE_HEIGHT_MAP=512
 SIZE_DIFFUSION_MAP=4096
 
-# Expected error message for unsorted DSM:
-# ERROR 1: Ungridded dataset: At line <LINE_NUMBER>, change of Y direction
-REGEX_ERROR_UNGRIGGED_DATASET=".*Ungridded dataset.*"
+DIR_TMP=tmp
+DIR_OUTPUT=output
 
 # Expected dgalinfo string output pattern for size info.
 REGEX_SIZE=".*Size is ([0-9]+), ([0-9]+).*"
 
-# Stored inputted DSM filename into a variable.
-dsm_filename=$1
 
-#####################
-# ORDERING DSM DATA #
-#####################
+###########
+# CLEANUP #
+###########
 
-printf "\nVerifying if DSM is ordered...\n"
-# Execute gdalinfo command and capture error output.
-gdalinfo_stdout="$(gdalinfo $dsm_filename 2>&1)"
+# Delete temporary files that the script creates.
+# They may be left over from abruptly stopping the script during a previous run.
+if [ -d $DIR_TMP ]; then rm -Rf $DIR_TMP; fi
 
+# Recreate the tmp directory where temporary files will be saved.
+mkdir $DIR_TMP
 
-if [[ $gdalinfo_stdout =~ $REGEX_ERROR_UNGRIGGED_DATASET ]]; then
-  # Create an ordered DSM file if the error output indicates that the DSM is not ordered.
-  printf "DSM is not ordered. Ordering...\n"
-
-  # Build name of new file with ordered data.
-  extension="${dsm_filename##*.}"
-  dsm_filename="${dsm_filename//\.xyz/.ordered.}$extension"
-
-  # Order data and save in the new file.
-  sort -k2 -n -k1 $1 -o $dsm_filename
-
-  # Verify if ordering was successful by checking for gdalinfo error message on ordered DSM file.
-  printf "Validating ordered DSM...\n"
-  gdalinfo_stdout="$(gdalinfo $dsm_filename 2>&1)"
-  if [[ $gdalinfo_stdout =~ $REGEX_SIZE ]]; then
-      printf "DSM ordering was successful.\n"
-  else
-      printf "DSM ordering failed with the following error:\n\n"
-      printf "$gdalinfo_stdout"
-
-      printf "\n\nExiting program.\n\n"
-      exit 1
-  fi
-
-elif [[ $gdalinfo_stdout =~ $REGEX_SIZE ]]; then
-    # No errors indicates that the DSM is already ordered.
-    printf "DSM is ordered.\n"
-
-else
-  # In case of an unexpected error: display the error message and exit the program.
-  printf "An unexpected error occured:\n\n"
-  printf "$gdalinfo_stdout"
-
-  printf "\n\nExiting program.\n\n"
-  exit 1
-fi
+# Create output directory if it doesn't exist already.
+if [ ! -d output ]; then mkdir output; fi
 
 
-# Fetch width and height of the DSM.
+###################
+# CREATE DTM FILE #
+###################
+
+# Use LAStools to parse the xyzRGB pointcloud ASCII format data into a laz format file.
+printf "\nParsing xyzRGB pointcloud file to LAZ...\n"
+txt2las -parse xyzRGB -i $1 -o $DIR_TMP/pointcloud.laz
+
+# Use PDAL to create the GTiff DTM file.
+printf "\nCreating GTiff DTM...\n"
+pdal pipeline pipelines/dtm.json
+
+# Use GDAL to fetch the size of the DTM.
+gdalinfo_stdout="$(gdalinfo $DIR_TMP/dtm.tif 2>&1)"
+
+
+##################
+# FETCH DTM SIZE #
+##################
+
+# Use GDAL to fetch the width and height of the DTM.
 if [[ $gdalinfo_stdout =~ $REGEX_SIZE ]]
 then
     width="${BASH_REMATCH[1]}"
     height="${BASH_REMATCH[2]}"
-    #printf "DSM size is ${width}x${height}.\n"
 fi
+
+
+###############################
+# DEFINE SIZE OF OUTPUT FILES #
+###############################
+
+# Define the size of the heightmap output.
+# Either width or height needs to be set to SIZE_HEIGHT_MAP.
+# Pick the one that is the longest from the DTM size that was fetched earlier.
+if [ "$width" -gt "$height" ]; then
+    heightmap_outsize="$SIZE_HEIGHT_MAP 0"
+    diffusionmap_outsize="$SIZE_DIFFUSION_MAP 0"
+else
+    heightmap_outsize="0 $SIZE_HEIGHT_MAP"
+    diffusionmap_outsize="0 $SIZE_DIFFUSION_MAP"
+fi
+
 
 #######################
 # CREATING HEIGHT MAP #
 #######################
 
-# Set size of heightmap output.
-if [ "$width" -gt "$height" ]; then
-    heightmap_outsize="$SIZE_HEIGHT_MAP 0"
-else
-    heightmap_outsize="0 $SIZE_HEIGHT_MAP"
-fi
-
-# Generate grayscale heightmap file.
+# Verbosity.
 printf "Creating grayscale heightmap...\n"
-gdal_translate -of PNG -ot Byte -scale $dsm_filename heightmap.png -outsize $heightmap_outsize
 
-# Use ImageMagick to rescaling canvas size to SIZE_HEIGHT_MAP x SIZE_HEIGHT_MAP pixels.
+# Use GDAL to create grayscale heightmap file.
+gdal_translate -of PNG -ot Byte -scale -b 1 $DIR_TMP/dtm.tif $DIR_OUTPUT/heightmap.png -outsize $heightmap_outsize
+
+# Use ImageMagick to rescale canvas size to SIZE_HEIGHT_MAP x SIZE_HEIGHT_MAP pixels.
 printf "Rescaling heightmap canvas to ${SIZE_HEIGHT_MAP}, ${SIZE_HEIGHT_MAP}...\n"
-#printf "Rescaling height canvas to 512x512px...\n"
+convert $DIR_OUTPUT/heightmap.png -resize "${SIZE_HEIGHT_MAP}x${SIZE_HEIGHT_MAP}" -background Black -gravity center -extent "${SIZE_HEIGHT_MAP}x${SIZE_HEIGHT_MAP}" heightmap.png
 
-# Use ImageMagick to rescale the heightmap image.
-convert heightmap.png -resize "${SIZE_HEIGHT_MAP}x${SIZE_HEIGHT_MAP}" -background Black -gravity center -extent "${SIZE_HEIGHT_MAP}x${SIZE_HEIGHT_MAP}" -out.png
+
+##########################
+# CREATING DIFFUSION MAP #
+##########################
+
+# Verbosity.
+printf "Creating RGB diffusionmap...\n"
+
+# Use PDAL to create the GTiff DTM file.
+pdal pipeline pipelines/rgb.json
+
+# Use python-gdal to merge red, green, and blue tif files into a single rbg.tif file.
+# This could probably have been done in the PDAL rgb pipeline filters.merge but it throws a malloc error for high resolutions.
+gdal_merge.py -separate $DIR_TMP/red.tif $DIR_TMP/green.tif $DIR_TMP/blue.tif -o $DIR_TMP/rgb.tif
+
+# Use GDAL to create the RGB diffusion map.
+gdal_translate -of PNG -ot Byte -scale -b 1 -b 2 -b 3 $DIR_TMP/rgb.tif $DIR_OUTPUT/diffusion.png -outsize $diffusionmap_outsize
+
+# Use ImageMagick to rescale canvas size to SIZE_DIFFUSION_MAP x SIZE_DIFFUSION_MAP pixels.
+printf "Rescaling heightmap canvas to ${SIZE_DIFFUSION_MAP}, ${SIZE_DIFFUSION_MAP}...\n"
+convert $DIR_OUTPUT/diffusion.png -resize "${SIZE_DIFFUSION_MAP}x${SIZE_DIFFUSION_MAP}" -background Black -gravity center -extent "${SIZE_DIFFUSION_MAP}x${SIZE_DIFFUSION_MAP}"  diffusion.png
+
+
+###########
+# CLEANUP #
+###########
+
+# Delete tmp directory containing all the temporary files created by the script.
+#rm -Rf tmp
